7a3ac356379f448e42a4954eb57e1e6aa75ff4fe
Shubh152 <shubhj013@gmail.com>
2025-02-16 21:50:43 +0530

    inital commit

---
+++ src/diff_car_model/README.md
@@ -0,0 +1,133 @@
+## Autonomous Navigation with Obstacle Avoidance
+
+This package provides a **simple differential drive robot** that can autonoumously navigate its surroundings and perform obstacle detection and avoidance using **Nav2 on a pre-generated map**. You can also generate a map using the bot by driving it around the surroundings (using slam_toolbox).
+
+### Software Stack and Packages Used
+
+- **ROS2 Jazzy on Ubuntu 24.04.1 LTS**
+- **Mapping using LiDAR-SLAM** : `slam_toolbox`
+- **Autonoumous Navigation and Obstacle Avoidance** : `Nav2`
+- **Localization** : `nav2_amcl`
+- **Sensor Fusion**: `robot_localization`
+- **Simulation** : `Gazebo Sim`
+- **URDF and State Publishing**: `joint_state_publisher` and `robot_state_publisher`
+
+---
+
+### Setup
+
+```bash
+cd ~
+mkdir -p ros2_ws/src
+cd ros2_ws/src
+git clone <repository_url>
+cd ..
+colcon build
+. install/setup.bash
+```
+
+The package is now ready for use
+
+---
+
+### Mapping the Area Using the Bot
+
+1. **Launch the robot model :**
+
+```
+ros2 launch diff_car_model map.launch.py
+```
+
+2. **Launch SLAM Toolbox** in a separate terminal:
+
+```bash
+ros2 launch slam_toolbox online_async_launch.py use_sim_time:=True slam_params_file:=~/ros2_ws/src/diff_car_model/config/slam_toolbox.yaml
+```
+
+3. **Control the robot manually**: In another terminal, use the `teleop_twist_keyboard` package to drive the robot in the Gazebo simulation:
+
+```
+ros2 run teleop_twist_keyboard teleop_twist_keyboard
+```
+
+Follow the on-screen instructions to operate the bot using your keyboard and drive it around to generate a map.
+
+4.  **Save the map :**
+
+- Open the SLAM Toolbox panel in RViz.
+- Click the "Save Map" button to save the generated map.
+
+---
+
+### Navigating the Pre-Generated Map Using the Bot
+
+1. **Launch the navigation stack:**
+
+```bash
+source install/setup.bash
+ros2 launch diff_car_model nav.launch.py
+```
+
+2. **Set the initial pose:**
+
+   - Open RViz.
+   - Use the `2D Pose Estimate` button to set the robot's initial pose on the map (this updates the `/initial_pose` topic).
+
+3. **Set a goal pose:**
+
+   - Use the `Nav2 Goal` button in RViz to set a goal position (this updates the `/goal_pose` topic).
+
+The robot will navigate to the goal while avoiding both static and dynamic obstacles.
+
+---
+
+### Working of the stack
+
+The bot operates using the following key functionalities:
+
+#### 1. Sensor Fusion
+
+The robot integrates data from multiple sensors to refine odometry and reduce drift. Specifically:
+
+- **Sensors used**:
+
+  - 2D LiDAR
+  - Wheel encoders
+  - Base IMU
+
+- **Tool used**: `robot_localization`
+- **Config file**: Located in `config/ekf.yaml`
+- **How it works**:
+
+  - Wheel encoder data provides relative displacement information.
+  - IMU provides orientation and angular velocity.
+  - The Extended Kalman Filter (EKF) in `robot_localization` fuses this data to produce accurate and drift-minimized odometry.
+
+#### 2. Localization
+
+Localization is handled by `nav2_amcl`:
+
+- **How it works**:
+
+  - The Adaptive Monte Carlo Localization (AMCL) algorithm uses the LiDAR scan and pre-generated map to localize the robot.
+  - It continually updates the robot's position in the map frame by comparing the scan data to the map.
+
+#### 3. Path Planning
+
+Path planning is performed by the Nav2 stack:
+
+- **Planner**:
+
+  - A global planner generates the optimal path from the robot’s current position to the goal pose.
+  - A local planner refines this path in real-time to account for dynamic obstacles.
+
+- **Config file**: Located in `config/nav2_params.yaml`
+
+#### 4. Obstacle Avoidance
+
+The robot uses `nav2_collision_monitor` to avoid obstacles:
+
+- **How it works:**
+
+  - Uses user-defined polygon around the bot and other areas of interest to look for obstacles inside the polygon and command the robot to either stop or slow down
+  - Polygons are also used to navigate around obstacles when the robot is stuck in a path and faces risk of colliding to an obstacle.
---
+++ src/diff_car_model/launch/map.launch.py
@@ -0,0 +1,179 @@
+import launch
+from launch.substitutions import Command, LaunchConfiguration
+import launch_ros
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='diff_car_model').find('diff_car_model')
+    default_model_path = os.path.join(pkg_share, 'src/model/diff_car_description.urdf')
+    default_rviz_config_path = os.path.join(pkg_share, 'rviz/nav2_config.rviz')
+
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])},{'use_sim_time' : True}]
+    )
+    joint_state_publisher_node = launch_ros.actions.Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', default_model_path])},{'use_sim_time' : True}],
+        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+    )
+    joint_state_publisher_gui_node = launch_ros.actions.Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
+    )
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig'), "--ros-args", "--remap", "use_sim_time:=true"],
+    )
+
+    pkg_slam = get_package_share_directory('slam_toolbox')
+    slam_launch_path = PathJoinSubstitution([pkg_slam, 'launch', 'online_async_launch.py'])
+    # ros2 launch slam_toolbox online_async_launch.py slam_params_file:=/home/shubh/diff_drive_car/diff_car_model/config/slam_toolbox.yaml
+
+
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    # pkg_spaceros_gz_sim = get_package_share_directory('spaceros_gz_sim')
+    gz_launch_path = PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])
+    gz_model_path = PathJoinSubstitution([pkg_share, 'src/model'])
+    gz_world_path = PathJoinSubstitution([gz_model_path, 'car_world.sdf'])
+
+    ros_gz_bridge_lidar = launch_ros.actions.Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=["/base_lidar@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan"],
+            remappings=[],
+            output='screen',
+            parameters=[{'use_sim_time' : True}]
+        )
+
+    ros_gz_bridge_imu = launch_ros.actions.Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=["/base_imu@sensor_msgs/msg/Imu@gz.msgs.IMU"],
+            remappings=[],
+            output='screen',
+            parameters=[{'use_sim_time' : True}]
+        )
+    
+    ros_gz_bridge_odom = launch_ros.actions.Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=["model/sam_bot/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry"],
+            remappings=[],
+            output='screen',
+            parameters=[{'use_sim_time' : True}]
+        )
+    
+    ros_gz_bridge_tf = launch_ros.actions.Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=["--ros-args", "-p", "config_file:=/home/shubh/diff_drive_car/diff_car_model/config/ros_gz_bridge_tf.yaml"],
+            remappings=[],
+            output='screen',
+            parameters=[{'use_sim_time' : True}]
+        )
+    
+    ros_gz_bridge_clock = launch_ros.actions.Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=["--ros-args", "-p", "config_file:=/home/shubh/diff_drive_car/diff_car_model/config/ros_gz_clock.yaml"],
+            remappings=[],
+            output='screen',
+            parameters=[{'use_sim_time' : True}]
+        )
+    
+    ros_gz_bridge_teleop = launch_ros.actions.Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=["cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist"],
+            remappings=[],
+            output='screen',
+            parameters=[{'use_sim_time' : True}]
+        )
+
+    robot_localization_node = launch_ros.actions.Node(
+       package='robot_localization',
+       executable='ekf_node',
+       name='ekf_filter_node',
+       output='screen',
+       parameters=[os.path.join(pkg_share, 'config/ekf.yaml'),{'use_sim_time' : True}]
+    )
+
+    broadcast_tf_static_lidar = launch_ros.actions.Node(
+       package='tf2_ros',
+       executable='static_transform_publisher',
+       name='static_transform_publisher',
+       output='screen',
+       arguments=["0", "0", "0", "0", "0", "0", "lidar_link", "sam_bot/base_link/lidar"],
+        parameters=[{'use_sim_time' : True}]
+    )
+
+    broadcast_tf_static_imu = launch_ros.actions.Node(
+       package='tf2_ros',
+       executable='static_transform_publisher',
+       name='static_transform_publisher',
+       output='screen',
+       arguments=["0", "0", "0", "0", "0", "0", "base_link", "sam_bot/base_link/imu_sensor"],
+            parameters=[{'use_sim_time' : True}]
+    )
+
+
+    return launch.LaunchDescription([
+        
+        ros_gz_bridge_clock,
+        ros_gz_bridge_lidar,
+        ros_gz_bridge_imu,
+        ros_gz_bridge_odom,
+        ros_gz_bridge_tf,
+        ros_gz_bridge_teleop,
+
+        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
+                                            description='Flag to enable joint_state_publisher_gui'),
+        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                            description='Absolute path to robot urdf file'),
+        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+                                            description='Absolute path to rviz config file'),
+
+        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
+                                            description='Flag to enable use_sim_time'),
+
+        launch.actions.DeclareLaunchArgument(
+            'world',
+            default_value=gz_world_path,
+            description='World to load into Gazebo'
+        ),
+        launch.actions.SetLaunchConfiguration(name='world_file', value=[LaunchConfiguration('world')]),
+        launch.actions.SetEnvironmentVariable('GZ_SIM_RESOURCE_PATH', gz_model_path),
+        launch.actions.IncludeLaunchDescription( 
+            PythonLaunchDescriptionSource(gz_launch_path),
+            launch_arguments={
+                'gz_args': [LaunchConfiguration('world_file')],
+                'on_exit_shutdown': 'True'
+            }.items(),
+        ),
+        # launch.actions.IncludeLaunchDescription( 
+        #     PythonLaunchDescriptionSource(slam_launch_path),
+        #     launch_arguments={
+        #         'use_sim_time' : "True",
+        #         'params_file' : '/home/shubh/diff_drive_car/diff_car_model/config/slam_toolbox.yaml'
+        #     }.items(),
+        # ),
+        joint_state_publisher_node,
+        # joint_state_publisher_gui_node,
+        robot_state_publisher_node,
+        robot_localization_node,
+        broadcast_tf_static_lidar,
+        broadcast_tf_static_imu,
+        rviz_node,
+    ])
